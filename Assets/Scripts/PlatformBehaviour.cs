e2f977a439641dc57c09049c73bb6f9b4445cebe
novalain <michael.f.noven@gmail.com>
2020-06-12 23:46:41 +0200

    Platform dropping, a bit wonky

---
+++ Assets/Scripts/PlatformBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehaviour : MonoBehaviour
+{
+    [SerializeField] private PlatformEffector2D m_effector;
+
+    private bool m_isDropping = false;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.S) && !m_isDropping)
+        {
+            m_isDropping = true;
+            m_effector.rotationalOffset = 180f;
+            StartCoroutine(RotateBackEffectorAfterDelay(0.2f));
+        }
+
+        if (Input.GetKeyUp(KeyCode.S) && Mathf.Approximately(m_effector.rotationalOffset, 0f))
+        {
+            m_isDropping = false;
+        }
+    }
+
+
+    private IEnumerator RotateBackEffectorAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        m_effector.rotationalOffset = 0f;
+    }
+
+}
