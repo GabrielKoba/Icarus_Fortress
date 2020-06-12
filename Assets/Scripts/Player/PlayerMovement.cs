dd6ceef88adfc8ab80d566bcb6ae93c79b488a97
novalain <michael.f.noven@gmail.com>
2020-06-12 22:43:34 +0200

    Implement player movement and jump

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController2D m_controller;
+    [SerializeField] private float m_runSpeed = 40f;
+
+    private float m_horizontalMove = 0f;
+    private bool m_jump = false;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        m_horizontalMove = Input.GetAxisRaw("Horizontal") * m_runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        m_controller.Move(m_horizontalMove * Time.fixedDeltaTime, false, m_jump);
+        m_jump = false;
+    }
+}
