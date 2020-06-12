a8b4eb2c85553937a482c860dceb6e1009a134de
novalain <michael.f.noven@gmail.com>
2020-06-12 23:15:20 +0200

    Put tweakable stuff in game config

---
+++ Assets/Scripts/Config/GameConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DefaultGameConfig", menuName = "ScriptableObjects/GameConfig", order = 1)]
+public class GameConfig : ScriptableObject
+{
+    public float PlayerMoveSpeed = 100f;
+    public float PlayerJumpForce = 600f;
+    public float PlayerGravity = 3f;
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,16 +2,23 @@
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private GameConfig m_gameConfig;
     [SerializeField] private CharacterController2D m_controller;
-    [SerializeField] private float m_runSpeed = 40f;
+    [SerializeField] private Rigidbody2D m_rigidBody;
 
     private float m_horizontalMove = 0f;
     private bool m_jump = false;
 
+    private void Start()
+    {
+        m_controller.m_JumpForce = m_gameConfig.PlayerJumpForce;
+        m_rigidBody.gravityScale = m_gameConfig.PlayerGravity;
+    }
+
     // Update is called once per frame
     private void Update()
     {
-        m_horizontalMove = Input.GetAxisRaw("Horizontal") * m_runSpeed;
+        m_horizontalMove = Input.GetAxisRaw("Horizontal") * m_gameConfig.PlayerMoveSpeed;
 
         if (Input.GetButtonDown("Jump"))
         {
