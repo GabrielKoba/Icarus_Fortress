24fba03e2146ffcf104069e9da42880ad1a26da7
novalain <michael.f.noven@gmail.com>
2020-06-13 00:21:40 +0200

    Make player pickup cannon ball from pile

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,13 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private GameConfig m_gameConfig;
     [SerializeField] private CharacterController2D m_controller;
     [SerializeField] private Rigidbody2D m_rigidBody;
+    [SerializeField] private GameObject m_heldCannonBall;
 
     private float m_horizontalMove = 0f;
     private bool m_jump = false;
+    private bool m_canPickupCannonBall = false;
+
+    private const string BALL_PILE_COLLIDER_TAG = "BallPile";
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(BALL_PILE_COLLIDER_TAG))
+        {
+            m_canPickupCannonBall = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag(BALL_PILE_COLLIDER_TAG))
+        {
+            m_canPickupCannonBall = false;
+        }
+    }
 
     private void Start()
     {
@@ -15,7 +36,6 @@ public class PlayerMovement : MonoBehaviour
         m_rigidBody.gravityScale = m_gameConfig.PlayerGravity;
     }
 
-    // Update is called once per frame
     private void Update()
     {
         m_horizontalMove = Input.GetAxisRaw("Horizontal") * m_gameConfig.PlayerMoveSpeed;
@@ -23,6 +43,11 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             m_jump = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && m_canPickupCannonBall)
+        {
+            m_heldCannonBall.SetActive(true);
         }
     }
 
