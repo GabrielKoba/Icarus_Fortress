0b816a4dc3b36356ccd011e76d51e400d7e02fb9
GabrielKoba <koberagabriel@gmail.com>
2020-06-12 16:25:02 -0300

    Initial Commit

---
+++ Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScrolling : MonoBehaviour {
+
+    [Header("Settings")]
+    [SerializeField] float scrollingSpeed = 1f;
+    [SerializeField] Vector3 loopStartPosition;
+    [SerializeField] Vector3 loopEndPosition;
+
+    void Update() {
+        Scroll();
+    }
+
+    void Scroll() {
+        transform.position = Vector3.MoveTowards(transform.position, loopEndPosition, scrollingSpeed * Time.deltaTime);
+
+        if (transform.position.x <= loopEndPosition.x) {
+            transform.position = new Vector3 (loopStartPosition.x, loopStartPosition.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/SpriteBounce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteBounce : MonoBehaviour {
+
+    [SerializeField]float frequency = 1.0f;  // Speed of sine movement
+    [SerializeField]float magnitude = 0.01f;   // Size of sine movement
+    private Vector3 axis;
+    private Vector3 pos;
+
+    void Start() {
+        pos = transform.position;
+        axis = transform.up;
+    }
+
+    void Update() {
+        transform.position = pos + axis * Mathf.Sin (Time.deltaTime * frequency) * magnitude;        
+    }
+}
