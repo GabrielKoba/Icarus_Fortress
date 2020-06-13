10eefeead89443f34913c5c6c7d89faf633b4458
novalain <michael.f.noven@gmail.com>
2020-06-13 19:01:52 +0200

    Add pixel font, add simple boot scene

---
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private string m_sceneToLoad;
+
+    private bool m_textIsComplete = false;
+
+    private void LoadScene(string sceneName)
+    {
+        StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.In, sceneName));
+    }
+
+    public void OnTextComplete()
+    {
+        m_textIsComplete = true;
+    }
+
+    public void Update()
+    {
+        if (m_textIsComplete && Input.GetMouseButtonDown(0))
+        {
+            LoadScene(m_sceneToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UITypeWriter.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UITypeWriter : MonoBehaviour
+{
+    [SerializeField] private Text m_text;
+    [SerializeField] private float m_delayBeforeStart;
+    [SerializeField] private float m_delay = 0.125f;
+    [SerializeField] private bool m_allowSkip;
+    [SerializeField] private UnityEvent m_onTextComplete;
+
+    private string m_story;
+
+    private void Awake ()
+    {
+        m_story = m_text.text;
+        m_text.text = string.Empty;
+
+        StartCoroutine(PlayText());
+    }
+
+    private IEnumerator PlayText()
+    {
+        yield return new WaitForSeconds(m_delayBeforeStart);
+
+        foreach (char c in m_story)
+        {
+            m_text.text += c;
+
+            if (m_delay > 0f)
+            {
+                yield return new WaitForSeconds (m_delay);
+            }
+        }
+
+        m_onTextComplete.Invoke();
+    }
+
+    public void Update()
+    {
+        if (m_allowSkip && Input.GetMouseButtonDown(0))
+        {
+            m_delay = 0f;
+        }
+    }
+
+}
