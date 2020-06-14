e6f54d747360d7c90a82114b2c1e44d289269d13
novalain <michael.f.noven@gmail.com>
2020-06-14 22:59:10 +0200

    Remove debug logs

--- Assets/Scripts/ProgressBarBehaviour.cs
+++ Assets/Scripts/ProgressBarBehaviour.cs
@@ -22,17 +22,12 @@ public class ProgressBarBehaviour : MonoBehaviour
     {
         var distance = Mathf.Abs(m_startPos.localPosition.x - m_endPos.localPosition.x);
 
-        Debug.Log("TOTAL dist" + distance);
-
         while (m_startPos.localPosition.x <= m_endPos.localPosition.x)
         {
             m_time += Time.deltaTime;
 
             var factor = m_time / m_config.TimeBeforeBossSpawn;
             var currentDistance = distance * factor;
-
-            Debug.Log("Factor " + factor);
-            Debug.Log("Distance " + currentDistance);
 
             m_startPos.transform.localPosition = new Vector3(m_startVector.x + currentDistance, m_startPos.transform.localPosition.y, m_startPos.transform.localPosition.z);
             yield return null;
